ddac44654c46697bfeab7aa7733c9fdf90976be5
ananta khodake <ananta.khodake02@gmail.com>
2024-01-11 14:22:35 +0530

    advertise component is compleate.

---
+++ hotstar-frontend/src/component/Adv.js
@@ -0,0 +1,100 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+function Adv({src,movie,date}) {
+  const Advcont = styled(Box)`
+    height: 15rem;
+    z-index:1;
+    width: 75rem;
+    display: flex;
+    margin-left:5rem;
+    gap:6rem;
+    padding: 10px;
+    margin-top:2rem;
+    justify-content:flex-start;
+    border-radius:8px;
+    align-items: center;
+    background:linear-gradient(270deg,#16181f,#0f1014);
+  `;
+  const Videocont = styled(Box)`
+    display: flex;
+    z-index:1;
+    overflow:hidden;
+    border-radius:18px;
+    height: 14.5rem;
+    width: 40rem;
+  `;
+  const Advtext= styled(Box)`
+    display:flex;
+    padding:5px;
+    justify-content;center;
+    align-items;center;
+    flex-direction:column;
+  `
+  const Advtextcont = styled(Box)`
+  display:flex;
+  gap:5px;
+  `;
+  const Advcontainer = styled(Box)`
+  height:12rem;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+
+  `
+  return (
+    <Advcont>
+      <Videocont>
+        <video
+          style={{ height: "100%", width: "100%", objectFit: "cover" }}
+          autoPlay="true"
+          loop={"true"}
+
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+        ></video>
+      </Videocont>
+      <Advcontainer>
+      <Advtextcont>
+      <Box
+        component="img"
+        src={src}
+        alt=""
+        style={{height:"50px", Width:"2"}}
+      />
+      <Advtext>
+      <Typography sx={{
+        color:"white",
+        fontFamily:"sans-serif",
+        fontSize:"14px",
+        lineHeight:"20px",
+        fontWeight:"500",
+
+      }} variant="h6" >{movie}</Typography>
+      <Typography sx={{
+        color:"#8f98b2",
+        fontSize:"12px",
+        fontWeight:"400",
+        lineHeight:"18px",
+        fontFamily:"sans-serif",
+      }} variant="h6" >Streming from {date}</Typography>
+      </Advtext>
+      </Advtextcont>
+      <Button sx={{
+        borderRadius:"8px",
+        width:"120px",
+        fontSize:"14px",
+        backgroundColor:"#252833",
+        textTransform:"capitalize",
+        "&:hover":{
+            background:"#252833",
+            transition:"0.02",
+            transform:"scale(1.1)"
+        }
+      }} variant="contained">Teaser</Button>
+      </Advcontainer>
+    </Advcont>
+  );
+}
+
+export default Adv;
--- hotstar-frontend/src/pages/home/Home.js
+++ hotstar-frontend/src/pages/home/Home.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import Sidenav from '../../component/Navbar/Sidenav'
-import styled from '@emotion/styled'
-import { Box } from '@mui/material'
+import React from "react";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+import Adv from "../../component/Adv";
 
 function Home() {
-  const BodyWrapper = styled(Box)`
-  border: 2px solid red;
-  width:20%;
-  height:100%;
-  `
+  // // const BodyWrapper = styled(Box)`
+  // // border: 2px solid red;
+  // // width:20%;
+  // // height:100%;
+  // `
   return (
-    <h1>Home Test</h1>
-  )
+    <>
+      <Adv
+        src={
+          "https://brand-img1.hotstarext.com/image/upload/v1585728139/Disnet%20Plus%20Hotstar%20Logo/D_Hotstar_logo_Dark_BG_120x120.png"
+        }
+        movie={"Movie name"}
+        date={"janewari 6"}
+      />
+    </>
+  );
 }
 
-export default Home
+export default Home;
